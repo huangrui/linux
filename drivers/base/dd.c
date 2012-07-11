540f27fd526cfde0333ae3e9b1fc4897f0510eda
Huang Rui <ray.huang@amd.com>
2012-07-11 12:20:39 +0800

    Add to access for driver_attach
    Signed-off-by: Huang Rui <ray.huang@amd.com>

--- drivers/base/dd.c
+++ drivers/base/dd.c
@@ -460,6 +460,7 @@ static int __driver_attach(struct device *dev, void *data)
  */
 int driver_attach(struct device_driver *drv)
 {
+	printk(KERN_DEBUG "Access into driver_attach");
 	return bus_for_each_dev(drv->bus, NULL, drv, __driver_attach);
 }
 EXPORT_SYMBOL_GPL(driver_attach);
