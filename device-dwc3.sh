384a007a8eda45379ea7abd29e5dc7f3ac142c76
Huang Rui <ray.huang@amd.com>
2014-03-13 13:47:01 +0800

    add phy-nop in script
    Signed-off-by: Huang Rui <ray.huang@amd.com>

--- device-dwc3.sh
+++ device-dwc3.sh
@@ -1,11 +1,13 @@
 #make CFLAGS+=-g -j4
-sudo rmmod usb_f_ss_lb
 sudo rmmod g_zero
+sudo rmmod usb_f_ss_lb
 sudo rmmod libcomposite
 sudo rmmod dwc3
+sudo rmmod phy_nop
 sudo rmmod udc_core
 sudo rmmod dwc3_pci
 sudo dmesg -C
+sudo cp /home/amd/linux-usb/drivers/usb/phy/phy-nop.ko /lib/modules/3.10.20-dwc3-pci+/kernel/drivers/usb/phy/
 sudo cp /home/amd/linux-usb/drivers/usb/gadget/g_zero.ko /lib/modules/3.10.20-dwc3-pci+/kernel/drivers/usb/gadget/
 sudo cp /home/amd/linux-usb/drivers/usb/gadget/libcomposite.ko /lib/modules/3.10.20-dwc3-pci+/kernel/drivers/usb/gadget/
 sudo cp /home/amd/linux-usb/drivers/usb/gadget/usb_f_ss_lb.ko /lib/modules/3.10.20-dwc3-pci+/kernel/drivers/usb/gadget/
